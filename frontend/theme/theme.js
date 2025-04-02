40fb9387f61582158e6709a715dd2dd092c8b053
Kate <154476116+katiebaumeister@users.noreply.github.com>
2025-04-02 18:31:47 -0400

    Create theme.js

---
+++ frontend/theme/theme.js
@@ -0,0 +1,10 @@
+import { colors } from './theme/colors';
+
+export const theme = {
+  colors,
+  font: {
+    regular: 'Nunito-Regular',
+    semibold: 'Nunito-SemiBold',
+    bold: 'Nunito-Bold',
+  },
+};
