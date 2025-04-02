72ae2763b99052f1f83533f8dc20545277bb4970
Kate <154476116+katiebaumeister@users.noreply.github.com>
2025-04-02 18:00:58 -0400

    Update useFonts.js

--- frontend/hooks/useFonts.js
+++ frontend/hooks/useFonts.js
@@ -1,1 +1,21 @@
+import { useEffect, useState } from 'react';
+import * as Font from 'expo-font';
+
+export default function useFonts() {
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    async function loadFonts() {
+      await Font.loadAsync({
+        'Nunito-Regular': require('../assets/fonts/Nunito-Regular.ttf'),
+        'Nunito-SemiBold': require('../assets/fonts/Nunito-SemiBold.ttf'),
+        'Nunito-Bold': require('../assets/fonts/Nunito-Bold.ttf'),
+      });
+      setLoaded(true);
+    }
+    loadFonts();
+  }, []);
+
+  return loaded;
+}
 
