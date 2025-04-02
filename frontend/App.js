9057365093598f02e18d76ded3953a13fa180a4a
Kate <154476116+katiebaumeister@users.noreply.github.com>
2025-04-02 18:26:17 -0400

    Update App.js

--- frontend/App.js
+++ frontend/App.js
@@ -3,6 +3,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
 import PlannerScreen from './screens/PlannerScreen';
+import StudentListScreen from './screens/StudentListScreen';
+import AddStudentScreen from './screens/AddStudentScreen';
 import useFonts from './hooks/useFonts';
 import { StatusBar } from 'expo-status-bar';
 
@@ -18,6 +20,8 @@ export default function App() {
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Planner" component={PlannerScreen} />
+        <Stack.Screen name="Students" component={StudentListScreen} />
+        <Stack.Screen name="AddStudent" component={AddStudentScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
