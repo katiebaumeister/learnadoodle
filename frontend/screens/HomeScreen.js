10ae3afad38030c247defec57ce11172b96390e1
Kate <154476116+katiebaumeister@users.noreply.github.com>
2025-04-02 18:00:35 -0400

    Update HomeScreen.js

--- frontend/screens/HomeScreen.js
+++ frontend/screens/HomeScreen.js
@@ -1,1 +1,23 @@
+import React from 'react';
+import ScreenWrapper from '../components/ScreenWrapper';
+import CalendarBar from '../components/CalendarBar';
+import JoyCorner from '../components/JoyCorner';
+import LearningLogger from '../components/LearningLogger';
+import WeeklySummaryCard from '../components/WeeklySummaryCard';
+import ProgressRing from '../components/ProgressRing';
+import FloatingPlannerButton from '../components/FloatingPlannerButton';
 
+const HomeScreen = ({ navigation }) => {
+  return (
+    <ScreenWrapper>
+      <CalendarBar />
+      <ProgressRing />
+      <JoyCorner />
+      <WeeklySummaryCard />
+      <LearningLogger />
+      <FloatingPlannerButton onPress={() => navigation.navigate('Planner')} />
+    </ScreenWrapper>
+  );
+};
+
+export default HomeScreen;
