112031ac21be13deaf448d1b918631fb88def612
Kate <154476116+katiebaumeister@users.noreply.github.com>
2025-04-02 18:01:13 -0400

    Update firebaseConfig.js

--- frontend/services/firebaseConfig.js
+++ frontend/services/firebaseConfig.js
@@ -1,1 +1,14 @@
+import { initializeApp } from 'firebase/app';
 
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+export default firebaseApp;
