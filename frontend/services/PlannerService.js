18b122ba2c2e5cb44db975fe32aed6cde15e6319
Kate <154476116+katiebaumeister@users.noreply.github.com>
2025-04-03 15:59:58 -0400

    Update PlannerService.js

--- frontend/services/PlannerService.js
+++ frontend/services/PlannerService.js
@@ -7,3 +7,7 @@ export const triggerPlanner = async (family_id) => {
 export const getPlannerStatus = async (family_id) => {
   return await apiClient(`/api/planner_status?family_id=${family_id}`);
 };
+
+export const getCurriculum = async () => {
+  return await apiClient('/api/get_subjects_and_units', 'GET');
+};
