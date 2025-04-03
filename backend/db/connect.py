f04b526aa078f1337d8941efd24e2834d0e0bddc
Kate <154476116+katiebaumeister@users.noreply.github.com>
2025-04-03 17:16:47 -0400

    Update connect.py

--- backend/db/connect.py
+++ backend/db/connect.py
@@ -1,3 +1,5 @@
 import sqlite3
 conn = sqlite3.connect('learnadoodle.db', check_same_thread=False)
 cursor = conn.cursor()
+from sqlmodel import create_engine
+engine = create_engine("sqlite:///database.db")  # or use Postgres for production
