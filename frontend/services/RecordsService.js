6846c8a392b24d86b922ed8fa86803189c63b01d
Kate <154476116+katiebaumeister@users.noreply.github.com>
2025-04-02 22:53:02 -0400

    Update RecordsService.js

--- frontend/services/RecordsService.js
+++ frontend/services/RecordsService.js
@@ -3,3 +3,11 @@ import apiClient from './apiClient';
 export const getRecords = async (family_id) => {
   return await apiClient(`/api/get_records?family_id=${family_id}`);
 };
+
+export const generateJournal = async (family_id) => {
+  return await apiClient('/api/generate_journal', 'POST', { family_id });
+};
+
+export const generateReport = async (family_id) => {
+  return await apiClient('/api/generate_report', 'POST', { family_id });
+};
