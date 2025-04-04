d832ca1c080d2de8bb81f94b0ccef0b5198479a6
Kate <154476116+katiebaumeister@users.noreply.github.com>
2025-04-04 12:58:55 -0400

    Update joy.py

--- backend/api/joy.py
+++ backend/api/joy.py
@@ -1,9 +1,17 @@
 from fastapi import APIRouter, Depends
+from sqlmodel import Session, select
+from backend.db.connect import get_session
+from backend.db.models import Journal
 from backend.auth.firebase_auth import require_parent
 
 router = APIRouter()
 
-@router.get("/joy_corner")
-@require_parent()
-def joy_corner(family=Depends()):
-    return {"joy": "You are doing great!"}
+@router.get("/get_journal")
+async def get_journal(
+    family=Depends(require_parent),
+    session: Session = Depends(get_session)
+):
+    journal = session.exec(
+        select(Journal).where(Journal.family_id == family["family_id"])
+    ).all()
+    return journal
