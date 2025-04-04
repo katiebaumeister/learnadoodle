c2c1842f8e8ccbec7ebcbc7b83e1c0488fd35e5d
Kate <154476116+katiebaumeister@users.noreply.github.com>
2025-04-04 12:58:30 -0400

    Update utility.py

--- backend/api/utility.py
+++ backend/api/utility.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from backend.auth.firebase_auth import require_parent
 
 router = APIRouter()
 
-@router.get("/healthcheck")
-def healthcheck():
-    return {"status": "ok"}
-
+@router.get("/ping")
+async def ping(family=Depends(require_parent)):
+    return {"message": f"Hello {family['name_parent']}!"}
